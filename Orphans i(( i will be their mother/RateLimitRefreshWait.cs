0f02c3afe8a1af64dbbb2344ec364afef2fb4c41
Elliot\Kaci <kaci.yanova@bizmachine.com>
2018-03-31 02:52:53 +0200

    rate limit refresh method from the FB crawler that's obsolete now i redid it

---
+++ Orphans i(( i will be their mother/RateLimitRefreshWait.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Facebook.Objects;
+using Frakon.Utilities;
+
+namespace Facebook
+{
+    class RateLimitRefreshWait
+    {
+        public void WaitForRateLimitRefresh()
+        {
+            Random random = new Random();
+            var randomWaitMins = random.Next(55, 80);
+
+            DateTime waitUntil = DateTime.UtcNow.AddMinutes(randomWaitMins);
+            Log.Info($"Waiting for rate limit refresh until: {waitUntil}");
+
+            var parseCompaniesWhileWaiting = true;
+            while (true)
+            {
+                Log.Info($"Time left: {waitUntil - DateTime.UtcNow}");
+                ResultUpdater.FindUnparsedInDb();
+
+                if (parseCompaniesWhileWaiting)
+                {
+                    Log.Info($"Parsing companies while waiting for rate limit refresh...");
+                    ResultUpdater.FindUnparsedInDb();
+                    parseCompaniesWhileWaiting = false;
+                }
+
+                var randomSleepIntervalMins = random.Next(1, 10);
+                Thread.Sleep(randomSleepIntervalMins * 60 * 1000);
+                if (DateTime.UtcNow > waitUntil)
+                {
+                    waitUntil = DateTime.UtcNow.AddMinutes(randomWaitMins);
+                    PublicAccessHolder.tokenIndex = 0;
+                    break;
+                }
+            }
+        }
+    }
+}
